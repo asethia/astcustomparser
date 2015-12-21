22c506b3a8920304e7b17377a9d413824f71ca81
Arun Sethia <sethia.arun@gmail.com>
2015-12-21 10:48:48 +0530

    Update ASTFunctions.scala

--- src/main/scala/com/ast/functions/ASTFunctions.scala
+++ src/main/scala/com/ast/functions/ASTFunctions.scala
@@ -1,7 +1,7 @@
 package com.ast.functions
 
 /**
- * AST Expression Functions
+ * Hierarchical case class to deinfe AST tree nodes
  */
 trait ASTFunctions {
   sealed abstract class Expr
@@ -10,7 +10,10 @@ trait ASTFunctions {
   case class Mul(e1: Expr, e2: Expr) extends Expr
   case class Div(e1: Expr, e2: Expr) extends Expr
   case class Number(e: String) extends Expr
+  //Minimum function
   case class Min(e1: List[Expr]) extends Expr
+  //Maximum function
   case class Max(e1: List[Expr]) extends Expr
+  //varibale defination
   case class Var(e1: String) extends Expr
  }
