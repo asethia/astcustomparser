fc52077b59a000a8207813eb27a3de948aa708f6
Arun Sethia <sethia.arun@gmail.com>
2015-12-21 14:35:54 +0530

    Update ASTParser.scala

--- src/main/scala/com/ast/parser/ASTParser.scala
+++ src/main/scala/com/ast/parser/ASTParser.scala
@@ -8,13 +8,10 @@ import com.ast.functions._
  * Expression Parser
  */
 trait ASTParser extends JavaTokenParsers with ASTFunctions{
-  def exp_function:Parser[Expr]=expr | function | variable 
-  def function=("Max" ^^^Max |"Min" ^^^ Min) ~ "(" ~ repsep(exp_function,",") ~ ")" ^^ {case a~b~c~d=> a(c)}
-  def variable = ("var" ^^^ Var) ~ "(" ~ ident ~ ")"^^ {case v ~ a ~ n ~ b => v(n)}
-  def expr:Parser[Expr]=chainl1(term,"+" ^^^ Add | "-" ^^^ Sub)
-  def term=chainl1(factor,"*" ^^^ Mul | "/" ^^^ Div)
-  def factor=floatingPointNumber ^^ Number | "(" ~> exp_function <~ ")"
-
-  //this is for reference 
-  //("Max(" ~> repsep(expr,",") <~ ")" ^^ {case mname=>Max(mname)})
+  def exp_function: Parser[Expr] = expr | function | variable
+  def function = ("Max" ^^^ Max | "Min" ^^^ Min) ~ ("(" ~> repsep(exp_function, ",") <~ ")") ^^ { case f ~ list => f(list) }
+  def variable = ("var" ^^^ Var) ~ ("(" ~> ident <~ ")") ^^ { case v ~ n => v(n) }
+  def expr: Parser[Expr] = chainl1(term, "+" ^^^ Add | "-" ^^^ Sub)
+  def term = chainl1(factor, "*" ^^^ Mul | "/" ^^^ Div)
+  def factor = floatingPointNumber ^^ Number | "(" ~> exp_function <~ ")"
 }
