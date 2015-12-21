10289b2591302a86fe946a9c9094a19dbe60b750
Arun Sethia <sethia.arun@gmail.com>
2015-12-21 15:16:01 +0530

    Update ASTParser.scala
    added ifCondition, thenCondition and conditional

--- src/main/scala/com/ast/parser/ASTParser.scala
+++ src/main/scala/com/ast/parser/ASTParser.scala
@@ -8,9 +8,18 @@ import com.ast.functions._
  * Expression Parser
  */
 trait ASTParser extends JavaTokenParsers with ASTFunctions{
-  def exp_function: Parser[Expr] = expr | function | variable
+  def exp_function: Parser[Expr] = expr | function | variable | conditional
+  //functions
   def function = ("Max" ^^^ Max | "Min" ^^^ Min) ~ ("(" ~> repsep(exp_function, ",") <~ ")") ^^ { case f ~ list => f(list) }
+  //variable
   def variable = ("var" ^^^ Var) ~ ("(" ~> ident <~ ")") ^^ { case v ~ n => v(n) }
+  //condition
+  def conditional = ifCondition ~ thenCondition  ^^ { case ifcond ~ thenExp  => ifcond.copy(thenExp=thenExp)}
+  //if condition
+  def ifCondition = "(if|If)" ~ "(" ~> exp_function  ~ (">" | "<") ~ exp_function <~ ")"  ^^ { case e1 ~ cond ~ e2  => IfCondition(e1, e2, cond,null) }
+  //then result of if condition
+  def thenCondition= "(then|THEN|Then)" ~ (exp_function) ^^ {case then ~ expr=>expr} 
+  //expr
   def expr: Parser[Expr] = chainl1(term, "+" ^^^ Add | "-" ^^^ Sub)
   def term = chainl1(factor, "*" ^^^ Mul | "/" ^^^ Div)
   def factor = floatingPointNumber ^^ Number | "(" ~> exp_function <~ ")"
